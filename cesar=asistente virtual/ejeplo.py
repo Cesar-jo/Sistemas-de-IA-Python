2ac227a3d6b04b77eb96e18d5b5ef176a82dc5bf
César José V.R <68834511+Cesar-jo@users.noreply.github.com>
2021-11-21 12:38:28 -0600

    Add files via upload

---
+++ cesar=asistente virtual/ejeplo.py
@@ -0,0 +1,16 @@
+#lista = ['hola', 'hi', 'hellow', 'qué tal']       
+
+var = input("Ingrese algo : ")
+
+for i in ['hola', 'hi', 'hellow', 'qué tal']:
+    if var == i:
+        print("Hola, ¿Como estas?")
+        break
+
+    else:
+        print("Ingrese algo")
+        break
+
+
+
+    
